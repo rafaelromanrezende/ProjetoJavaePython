487c98ff78d3b9bac10ca9a243c2eafd6eda2f79
Rafael <rafaelromanrezende123@gmail.com>
2024-12-28 22:37:10 -0300

    Deu certo agora

--- PROJETOjava.java
+++ PROJETOjava.java
@@ -1,9 +1,6 @@
 public class PROJETOjava{
     public static void main(String[] args){
-        System.out.println("Ola mundo novo");
-        System.out.println(args[0]) ;
-        System.out.println("este é um teste2");
-        System.out.println("eu sou top");
+        System.out.println("Agora ta certo");
     }
     
 }
