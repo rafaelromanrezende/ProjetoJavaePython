dbdf30d8f078d11e0aeb08734688f7a855ebef6b
Rafael <rafaelromanrezende123@gmail.com>
2024-12-28 22:21:34 -0300

    Teste2

--- PROJETOjava.java
+++ PROJETOjava.java
@@ -2,7 +2,7 @@ public class PROJETOjava{
     public static void main(String[] args){
         System.out.println("Ola mundo novo");
         System.out.println(args[0]) ;
-        System.out.println("este é um teste!");
+        System.out.println("este é um teste2");
     }
     
 }
